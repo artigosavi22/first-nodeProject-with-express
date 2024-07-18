a910ae041e1eedc363087ac3ac8d5836574147a5
Arti Gosavi <artigosavi76@gmail.com>
2024-07-18 13:18:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app=express();
+
+// app.get('/',( req,res) =>{
+//     res.send("Hello World");
+// });
+//  const user=require("./api/routers/users/user");
+
+
+const PORT=3001;
+ app.listen(PORT,()=>{
+        console.log(`Server is running on ${PORT}`);
+ });
