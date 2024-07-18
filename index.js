04dd17cc87aea3a08eccbf652d6fb4123ff7abe4
Arti Gosavi <artigosavi76@gmail.com>
2024-07-18 14:15:03 +0530

    create Database Connection with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app=express();
 
+const mongoose= require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/invoice");
+
+const userSchema= new mongoose.Schema({
+        name:String,
+        mobile:Number,
+})
+
+const userModel= mongoose.model("users",userSchema);
+
+app.get('/getUsers',(req,res)=>{
+    userModel.find({}).then(function(users){
+        res.json(users);
+    }).catch(function(err){
+        console.log(err);
+    });
+})
 // app.get('/',( req,res) =>{
 //     res.send("Hello World");
 // });
